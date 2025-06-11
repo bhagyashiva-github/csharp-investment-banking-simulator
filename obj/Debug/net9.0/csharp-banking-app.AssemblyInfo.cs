954dc59ec9f1413d9dd95bcaf9fef6e63dd23ba8
BHAGYA SRI RAMKUMAR <bhagyasriramkumar@BHAGYAs-MacBook-Pro.local>
2025-06-11 14:51:23 +0800

    Initial commit

--- obj/Debug/net9.0/csharp-banking-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/csharp-banking-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp-banking-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15bb8d124057f6b2ea1f71b0a6715bd56f3a7fa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp-banking-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp-banking-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
